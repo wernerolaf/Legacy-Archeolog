b73b049bd1c9d9aa59b4f8859aad781ce21ac93f
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2023-12-09 11:12:16 +0100

    gpt-3.5 to gpt-4

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(
-    # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
     repo_path=os.environ.get("REPO_PATH"),
 )
@@ -17,7 +16,7 @@ chat_completion = client.chat.completions.create(
             "content": "Say this is a test",
         }
     ],
-    model="gpt-3.5-turbo",
+    model="gpt-4",
 )
 
 print(chat_completion)
