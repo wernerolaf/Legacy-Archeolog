5e4da48f8423003bec982cc271b9d4b919ef2550
wernerolaf <olafowerner@gmail.com>
2023-12-09 15:46:49 +0100

    get commits info

--- src/collector.py
+++ src/collector.py
@@ -2,3 +2,37 @@ import json
 from git import Repo
 
 
+def get_commit_info(repo_path, file_path=None):
+    repo = Repo(repo_path)
+
+    commits_info = []
+
+    for commit in repo.iter_commits(paths=file_path):
+        commit_info = {
+            "commit": commit.hexsha,
+            "author": f"{commit.author.name} <{commit.author.email}>",
+            "date": commit.authored_datetime.isoformat(),
+            "message": commit.message.strip(),
+            "changes": []
+        }
+
+        if len(commit.parents) > 0:
+            parent = commit.parents[0]
+        else:
+            parent = commit.parents
+            # Get code changes for each commit
+        for diff in commit.diff(parent, create_patch=True):
+            commit_info["changes"].append({
+                "filename": diff.a_path,
+                "additions": diff.diff.decode('utf-8', 'ignore').count('+'),
+                "deletions": diff.diff.decode('utf-8', 'ignore').count('-')
+            })
+
+        commits_info.append(commit_info)
+
+    return commits_info
+
+
+def save_to_json(data, output_file):
+    with open(output_file, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
